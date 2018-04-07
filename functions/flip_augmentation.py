446b97796e82750c701e10c70ab760bde8ef12f2
hitsgub <hitstales@gmail.com>
2018-04-07 20:17:58 +0900

    add functions.FlipAugmentation(), for augmentation in feature space.

---
+++ functions/flip_augmentation.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  7 20:11:52 2018
+
+@author: HITS
+"""
+from chainer.backends import cuda
+from chainer import function
+
+
+class FlipAugmentation(function.Function):
+    """intermediate augmentation by flipping."""
+    def __init__(self, axes=(3,)):
+        self.axes = axes
+
+    def forward(self, xs):
+        self.retain_inputs(())
+        x = xs[0]
+        xp = cuda.get_array_module(x)
+        for i in self.axes:
+            slices = [slice(None)] * i + [slice(None, None, -1)]
+            y = xp.concatenate((x, x[slices]), axis=0)
+        return y,
+
+    def backward(self, xs, gys):
+        gx = gys[0]
+        for i in reversed(self.axes):
+            N = gx.shape[0] // 2
+            gx0 = gx[:N]
+            slices = [slice(N, None)] + [slice(None)] * (i - 1) + \
+                [slice(None, None, -1)]
+            gx1 = gx[slices]
+            gx = gx0 + gx1
+        return gx,
